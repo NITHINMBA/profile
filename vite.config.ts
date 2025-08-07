2a123a94d9fb57f84f37f91084164329541a3c5b
nithin.github.io <158437882+NITHINMBA@users.noreply.github.com>
2025-08-07 11:00:19 +0000

    profile

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/profile/', // <-- This is important for GitHub Pages!
-  plugins: [react()],
-});
+  base: '/profile/',
+  build: {
+    outDir: 'dist'
+  }
+})
