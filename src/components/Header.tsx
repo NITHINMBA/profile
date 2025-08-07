71a8327a2f047484937dffc13d8b0fd6aa02e0de
nithin <nithincr71609@gmail.com>
2025-08-07 18:54:54 +0530

    hea

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,6 +15,7 @@ export const Header: React.FC = () => {
               className="w-32 h-32 sm:w-40 sm:h-40 mx-auto border-4 border-white dark:border-navy-700 shadow-xl object-cover"
             />
           </div>
+
           {/* Name and Title */}
           <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold text-navy-900 dark:text-white mb-4 animate-slide-up">
             {personalInfo.name}
