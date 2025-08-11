4e807a496b5ae8be631af10136b7ee20684625e8
nithin.github.io <158437882+NITHINMBA@users.noreply.github.com>
2025-08-11 13:06:05 +0000

    pc

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,7 +7,7 @@ export const personalInfo = {
   phone: "+91 9788546486",
   location: "Chennai, India",
   linkedin: "https://www.linkedin.com/in/nithinmc",
-  profileImage: "https://github.com/NITHINMBA/profile/blob/gh-pages/Photoff.jpg?raw=true"
+  profileImage: "https://raw.githubusercontent.com/NITHINMBA/profile/gh-pages/Photoff.jpg"
 };
 
 export const aboutInfo = {
